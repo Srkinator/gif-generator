0e397f5f799aa58d97a4cce5c4e477d94b4e39ca
Srkinator <srki2207@gmail.com>
2018-01-22 21:17:49 +0100

    setup of the app structure done

---
+++ src/components/main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class MainPage extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>radi</h1>        
+            </div>
+        );
+    }
+}
+
+export default MainPage;
