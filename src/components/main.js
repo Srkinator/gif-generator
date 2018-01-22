b47aabdba9f7f5266c400a541c8aaa63539f6b0f
Srkinator <srki2207@gmail.com>
2018-01-22 21:45:02 +0100

    communication service done

--- src/components/main.js
+++ src/components/main.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
+import { communication } from "../services/communicationService";
 
 class MainPage extends Component {
     constructor(props) {
         super(props);
         
     }
+
+    componentDidMount(){
+        this.loadGif();
+    }
+
+    loadGif = () => {
+        communication.getGif("lebron james", (response)=>{
+            console.log(response)
+        }, (error)=>{
+            console.log(error);
+        })
+    }
     
     render() {
         return (
             <div>
-                <h1>radi</h1>        
+                <h1>raasddi</h1>        
             </div>
         );
     }
---
+++ src/services/communicationService.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+class CommunicationService {
+    getGif(searchString, dataHandler, errorHandler) {
+		axios.get('https://api.giphy.com/v1/gifs/search', {
+			params: {
+				api_key: "lXfqqsnpNmFr6EnSSOY8x8AAhmmRu393",
+				q: searchString,
+				limit: 
+			}
+		})
+		.then((response) => {
+			dataHandler(response.data.data);
+		})
+		.catch((error) => {
+			errorHandler(error);
+		})
+	}
+}
+
+export const communication = new CommunicationService();
